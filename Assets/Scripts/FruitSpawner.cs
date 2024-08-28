cc7a83352b678059084c40084242c27fe40ad18b
ryeon <ryeon530@gmail.com>
2024-08-28 21:30:35 +0900

    feat: 세로축에 여러 개의 과일 스폰 처리. 작업 로그: https://velog.io/@ryeon/여러-개의-y-좌표에-과일-스폰하기

--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
@@ -8,11 +10,16 @@ public class FruitSpawner : MonoBehaviour
     private GameObject[] fruits;
     [SerializeField]
     private float spawnIntervalSecond;
+    [SerializeField]
+    private int maxFruitCount = 50;
+
+    private List<List<float>> yCoordsList;
 
     private string spawnFruitRoutineName = "SpawnFruitRoutine";
 
     void Start()
     {
+        InitYCoordsList();
         StartCoroutine(spawnFruitRoutineName);
     }
 
@@ -21,17 +28,38 @@ public class FruitSpawner : MonoBehaviour
 
     }
 
+    void InitYCoordsList()
+    {
+        yCoordsList = new List<List<float>>();
+        // yCoordsList.AddRange(Enumerable.Repeat(new List<float> { -5f }, 50)); // Enumerable.Repeat은 동일 객체를 참조함.
+        for (int i = 0; i < maxFruitCount; i++)
+        {
+            yCoordsList.Add(new List<float> { -5f });
+        }
+
+        System.Random random = new System.Random();
+        for (int i = 0; i < 10; i++)
+        {
+            int randomIndex = random.Next(0, yCoordsList.Count); // 마지막 요소는 포함되지 않음. 즉 50은 출력되지 않는다.
+            yCoordsList[randomIndex].Add(-1f);
+        }
+    }
+
     IEnumerator SpawnFruitRoutine()
     {
-        while (true)
+        foreach (List<float> yCoords in yCoordsList)
         {
-            SpawnFruit();
+            SpawnFruit(yCoords);
             yield return new WaitForSeconds(spawnIntervalSecond);
         }
     }
 
-    void SpawnFruit()
+    void SpawnFruit(List<float> yCoords)
     {
-        Instantiate(fruits[0], transform.position, Quaternion.identity); // 3번째 인자는 오브젝트의 회전을 설정함. identity: 회전 없음 / 기본 회전값(0,0,0)
+        foreach (float yCoord in yCoords)
+        {
+            // 3번째 인자는 오브젝트의 회전을 설정함. identity: 회전 없음 / 기본 회전값(0,0,0)
+            Instantiate(fruits[0], new Vector3(transform.position.x, yCoord, 0), Quaternion.identity);
+        }
     }
 }
